6394b812a1bc4d61a99314cc103290c709873938
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-01-25 13:53:55 -0600

    Add html and js script with basic operation functions

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function add(a, b){
+    return a+b;
+}
+
+function substract(a, b){
+    return a-b;
+}
+
+function multiply(a, b){
+    return a*b;
+}
+
+function divide(a, b){
+    return a/b;
+}
+
+console.log(add(4,2));
+console.log(substract(4,2));
+console.log(multiply(4,2));
+console.log(divide(4,2));
