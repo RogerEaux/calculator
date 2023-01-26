d587cfbe3c4c18dc5c756a052be6b3308f976106
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-01-25 21:43:33 -0600

    Add functions to handle operator clicks and to change bottom and tops display

--- js/script.js
+++ js/script.js
@@ -15,15 +15,39 @@ function operate(operator, fisrtNumber, secondNumber){
     }
 }
 
-function changeBottomDisplay(){
-    let displayBottom = '';
-    const numberButtons = document.querySelectorAll('.number');
-    numberButtons.forEach(numberButton => numberButton.addEventListener('click',() =>{
-        displayBottom += numberButton.textContent;
-        const bottom = document.querySelector('.bottom');
-        bottom.textContent = displayBottom;
-    }));
+function changeBottomDisplay(button){
+    const bottom = document.querySelector('.bottom');
+    bottom.textContent += button.textContent;
 }
 
-changeBottomDisplay();
+function changeTopDisplay(button){
+    const top = document.querySelector('.top');
+    const bottom = document.querySelector('.bottom');
+    top.textContent = bottom.textContent + button.textContent;
+    bottom.textContent = '';
+}
+
+function operatorClick(){
+    changeTopDisplay(this);
+    const operators = document.querySelectorAll('.operator');
+    operators.forEach(operator => operator.removeEventListener('click', operatorClick));
+}
+
+function numberClick(){
+    if(this.textContent === '.'){
+        const decimalPoint = document.querySelector('#point');
+        decimalPoint.removeEventListener('click',numberClick);
+    }
+    changeBottomDisplay(this);
+    const operators = document.querySelectorAll('.operator');
+    const top = document.querySelector('.top');
+    operators.forEach(operator => operator.addEventListener('click', operatorClick));
+}
+
+function setUp(){
+    const numberButtons = document.querySelectorAll('.number');
+    numberButtons.forEach(numberButton => numberButton.addEventListener('click', numberClick));
+}
+
+setUp();
 console.log(operate('divide', 2, 4));
