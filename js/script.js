6058cd818bc34d38f18c091efecb6d4ef50d1c6a
Roberto Meré <121988408+RogerEaux@users.noreply.github.com>
2023-01-26 20:54:15 -0600

    Change operate and operatorClick functions for desired behaviour

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
-add = (a, b) => a+b;
-substract = (a, b) => a-b;
-multiply = (a, b) => a*b;
-divide = (a, b) => a/b;
-
-function operate(operator, fisrtNumber, secondNumber){
-    if(operator === 'add'){
-        return add(fisrtNumber, secondNumber);
-    }else if(operator === 'substract'){
-        return substract(fisrtNumber, secondNumber);
-    }else if(operator === 'multiply'){
-        return multiply(fisrtNumber, secondNumber);
-    }else if(operator === 'divide'){
-        return divide(fisrtNumber, secondNumber);
+function operate(){
+    const elements = getOperationElements();
+    const operator = elements[0];
+    const a = elements[1];
+    const b = elements[2];
+    if(operator === '+'){
+        return a + b;
+    }else if(operator === '-'){
+        return a - b;
+    }else if(operator === 'x'){
+        return a * b;
+    }else if(operator === '÷'){
+        return a / b;
     }
+}
+
+function getOperationElements(){
+    const top = document.querySelector('.top');
+    const bottom = document.querySelector('.bottom');
+    const operator = top.textContent.slice(-1);
+    const a = parseFloat(top.textContent.slice(0, -1));
+    const b = parseFloat(bottom.textContent);
+
+    return [operator, a, b];
 }
 
 function allowOperators(){
@@ -35,45 +44,46 @@ function disallowEquals(){
     equals.removeEventListener('click', equalsClick);
 }
 
-function changeBottomDisplay(button){
+function addToTopDisplay(button){
+    const top = document.querySelector('.top');
+    top.textContent += button.textContent;
+}
+
+function addToBottomDisplay(button){
     const bottom = document.querySelector('.bottom');
     bottom.textContent += button.textContent;
 }
 
-function changeTopDisplay(){
-    const top = document.querySelector('.top');
-    const bottom = document.querySelector('.bottom');
-    console.log(top.textContent);
-    if(!top.textContent.trim()){
-        top.textContent = bottom.textContent;
-    }else{
-        top.textContent = getOperationElements();
-    }
-    bottom.textContent = '';
-}
-
-function operatorClick(){
-    changeBottomDisplay(this);
-    changeTopDisplay();
-    disallowOperators();
-}
-
 function equalsClick(){
-    changeTopDisplay();
+
     disallowEquals();
 }
 
+function operatorClick(){
+    const top = document.querySelector('.top');
+    const bottom = document.querySelector('.bottom');
+    if(isNaN(top.textContent.slice(-1))){
+        top.textContent = operate();
+    }else if(bottom.textContent){
+        top.textContent = bottom.textContent;
+    }
+    bottom.textContent = '';
+    addToTopDisplay(this);
+    disallowOperators();
+}
+
 function numberClick(){
-    changeBottomDisplay(this);
+    addToBottomDisplay(this);
     allowOperators();
-    const top = document.querySelector('.top')
-    if(top.textContent){
+    const top = document.querySelector('.top');
+    if(isNaN(top.textContent.slice(-1))){
         allowEquals();
     }
     if(this.textContent === '.'){
         const decimalPoint = document.querySelector('#point');
         decimalPoint.removeEventListener('click',numberClick);
         disallowOperators();
+        disallowEquals();
     }
 }
 
